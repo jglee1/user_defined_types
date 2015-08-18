6d894e4998d0d3655fe1c97753c469a3c864a315
jglee1 <jglee1@gmail.com>
2015-08-18 19:54:17 -0400

    update

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 // 2nd change
 // 3rd change
+// from github
 
 struct Entry {
     string name;
@@ -17,9 +18,38 @@ ostream& operator<<(ostream& os, const Entry& e)
            e.number << "}";
 }
 
+istream& operator>>(istream& is, Entry& e)
+    // read { "name" , number } pair. Note: formatted with { "", and }
+{
+    char c, c2;
+    if (is>>c && c=='{' && is>>c2 && c2=='"') {  // start with a { "
+        string name;    // the default value of a string is the empty string: ""
+        while (is.get(c) && c!='"')     // anything before a " is part of the name
+            name += c;
+        if (is>>c && c==',') {
+            int number = 0;
+            if (is>>number>>c && c=='}') {  // read the number and a }
+                e = {name, number};    // assign to the entry
+                return is;
+            }
+         }
+
+     }
+   //  is.setf(ios_base::failbit);        // register the failure in the stream
+     return is;
+}
+
+void read_write()
+{
+    for (Entry ee; cin>>ee;)        // read from cin into ee
+        cout << ee << '\n';         // write ee to cout
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    read_write();
 
     return a.exec();
 }
